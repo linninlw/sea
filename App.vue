a843defd1d3263da1133313dd9437e16a125880b
li jinlin <493176693@qq.com>
2024-06-29 20:55:01 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,53 @@
+<!--
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+-->
+<!--
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Register</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+-->
+
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 0px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
